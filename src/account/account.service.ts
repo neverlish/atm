f58c9b272ed418403861a27deb4f0e76c673cdaf
Jinho Hyeon <neverlish@gmail.com>
2021-09-29 15:00:38 +0900

    added AccountService withdraw and deposit methods

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,5 +1,7 @@
-import { Account } from './account.interface';
+import { Account, Balance } from './account.interface';
 
 export interface AccountService {
   getAccounts(cardNumber: string): Promise<Account[]>;
+  withdraw(accountNumber: string, money: number): Promise<Balance>;
+  deposit(accountNumber: string, money: number): Promise<Balance>;
 }
--- src/account/account.serviceImpl.ts
+++ src/account/account.serviceImpl.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { BankingService } from 'src/banking/banking.service';
 import { AccountService } from './account.service';
 
 @Injectable()
 export class AccountServiceImpl implements AccountService {
+  @Inject('BankingServie') private bankingService: BankingService;
+
   async getAccounts(cardNumber: string) {
-    return [];
+    return await this.bankingService.getAccounts(cardNumber);
+  }
+
+  async withdraw(accountNumber: string, money: number) {
+    return await this.bankingService.withdraw(accountNumber, money);
+  }
+
+  async deposit(accountNumber: string, money: number) {
+    return await this.bankingService.deposit(accountNumber, money);
   }
 }
