6cedfcb9a1219919b40a95caf9f48e0471a1c92a
Jinho Hyeon <neverlish@gmail.com>
2021-09-29 09:24:01 +0900

    imported modules to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountModule } from './account/account.module';
+import { BankingModule } from './banking/banking.module';
+import { CardModule } from './card/card.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, BankingModule, CardModule],
   controllers: [],
   providers: [],
 })
