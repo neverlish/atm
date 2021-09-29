5442ec9e6b46ee4181e05236844f21e32dbd6606
Jinho Hyeon <neverlish@gmail.com>
2021-09-29 14:59:41 +0900

    added MockedAccount

--- src/account/account.interface.ts
+++ src/account/account.interface.ts
@@ -1,7 +1,33 @@
-type Balance = number;
+export type Balance = number;
 
 export interface Account {
   name: string;
   number: string;
+  cardNumber: string;
   balance: Balance;
+}
+
+export class MockedAccount implements Account {
+  name: string;
+  number: string;
+  cardNumber: string;
+  balance: Balance;
+
+  constructor(name, number, cardNumber, balance) {
+    this.name = name;
+    this.number = number;
+    this.cardNumber = cardNumber;
+    this.balance = balance;
+  }
+
+  deposit(money: number) {
+    this.balance += money;
+  }
+
+  withdraw(money: number) {
+    if (this.balance - money < 0) {
+      throw new Error('no money');
+    }
+    this.balance -= money;
+  }
 }
